31c79b1c7024256ed6b049d7b1e604be8d9a8f7c
vansh kalra <vanshkalra1379@gmail.com>
2024-07-21 00:14:22 +0530

    MongoDB Schema

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,3 +1,40 @@
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 
-const 
+
+
+// User Schema
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 5,
+        maxLength: 20,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 5
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 5
+    }
+});
+
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = {
+    User
+};
