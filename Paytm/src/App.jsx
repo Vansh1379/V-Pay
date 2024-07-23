165ce85d27fbf0ea2b6ebffed1e2542b41c4c33a
vansh kalra <vanshkalra1379@gmail.com>
2024-07-23 21:43:05 +0530

    Frontend Routes

--- Paytm/src/App.jsx
+++ Paytm/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 import './App.css'
+import SignUp from './components/SignUp/SignUp'
+import SignIn from './components/SignIn/SignIn'
+import Dashboard from './components/Dashboard/Dashboard'
+import SendMoney from './components/SendMoney/SendMoney'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,10 +13,10 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/signup' element={ } />
-          <Route path='/signin' element={ } />
-          <Route path='/dashboard' element={ } />
-          <Route path='/send' element={ } />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/send' element={<SendMoney />} />
         </Routes>
       </BrowserRouter>
     </>
