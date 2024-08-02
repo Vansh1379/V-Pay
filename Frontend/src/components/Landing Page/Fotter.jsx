10295d49ecc948eb115084bbd4b8a1cfa715ad68
vansh kalra <vanshkalra1379@gmail.com>
2024-08-02 06:17:41 +0530

    Paytm

--- Frontend/src/components/Landing Page/Fotter.jsx
+++ Frontend/src/components/Landing Page/Fotter.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Fotter() {
+const Fotter = () => {
+  const [businessSol, setBusinessSol] = useState([
+    { id: 1, name: 'Payment Gateway' },
+    { id: 2, name: 'Guardian By V pay' },
+    { id: 3, name: 'Express Checkout' },
+    { id: 4, name: 'V Pay Switch' },
+    { id: 5, name: 'Offline Merchant' },
+    { id: 6, name: 'Advertise on V pay' },
+    { id: 7, name: 'Smart Speaker' },
+    { id: 8, name: 'V Pay Loans' },
+    { id: 9, name: 'payment Links' },
+  ]);
   return (
-    <div>Fotter</div>
+    <div className='flex w-[1268px] h-max justify-evenly '>
+      <div className=''>
+        <div className='flex items-center gap-1.5'>
+          <div className='ml-2'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.0} stroke="#0c4b6f" className="size-8 ">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+          </div>
+          <h1 className='font-bold text-4xl text-sky-900 font-mono items-center'>V-Pay</h1>
+        </div>
+      </div>
+
+      <div className=''>
+        <div>
+          <h3 className='text-base font-medium mt-3 mb-2'>Bussiness Solution</h3>
+        </div>
+      </div>
+
+      <div className=''>
+        <div>
+          <h3 className='text-base font-medium mt-3 mb-2'>Insurence</h3>
+        </div>
+      </div>
+
+      <div className=''>
+        <div>
+          <h3>investments</h3>
+        </div>
+      </div>
+
+      <div className=''>
+        <div>
+          <h3>Genral</h3>
+        </div>
+      </div>
+
+      <div className=''>
+        <div>
+          <h3>Legal</h3>
+        </div>
+      </div>
+
+      <div className=''>
+        <div>
+          <h3>Support</h3>
+        </div>
+      </div>
+    </div>
   )
 }
 
